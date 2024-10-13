4e4314e89af674fc0c6eef6f6d94a682f84f7d8f
Bryan Chikwendu <26007246+bryanChikwendu@users.noreply.github.com>
2024-10-13 15:56:54 +0100

    Deployed AI

--- AI_Backend/main.py
+++ AI_Backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from models import User
 import google.generativeai as genai
 import os
@@ -6,7 +6,7 @@ from models import preamble
 genai.configure(api_key="AIzaSyDJ12I8qdTvBf0RDQ4DFl26gjXEZgjs7bY")
 
 app = FastAPI()
-
+print("Application Started")
 
 @app.get("/")
 async def root():
@@ -20,4 +20,4 @@ async def getItineraries(request: Request):
     response = model.generate_content(preamble + json_prefs.decode("utf-8"))
     formatted_resp = response.text.split("```")[1].strip("json").replace('\n', '')
     print(response.text)
-    return formatted_resp
+    return Response(content=formatted_resp, media_type='application/json')
