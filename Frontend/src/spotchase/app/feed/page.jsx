b16b9538721879fa95dd8620f40a0d76911bfb88
Bryan Chikwendu <26007246+bryanChikwendu@users.noreply.github.com>
2024-10-13 23:10:09 +0100

    Adding Final Features

--- Frontend/src/spotchase/app/feed/page.jsx
+++ Frontend/src/spotchase/app/feed/page.jsx
@@ -1,31 +1,12 @@
 "use client";
-import Link from "next/link";
-import axios from "axios";
 import { getSession } from "next-auth/react";
-import { useContext, useState, useEffect, useRef } from "react";
-import { isMobile } from "react-device-detect";
-import { motion } from "framer-motion";
-import { useRouter } from "next/navigation";
-import { ref, onValue } from "firebase/database";
-import { rdb } from "../../services/firebaseClient";
+import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../store/user-context";
-import { FeedbackContext } from "../../store/feedback-context";
 import { MatchesContext } from "../../store/matches-context";
-import { PlayerContext } from "../../store/player-context";
-import BoardingPage from "../../components/pages/BoardingPage";
-import ModalReport from "../../components/modals/ModalReport";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
-import DestinationCards from "../../components/DestinationCards";  // Import your DestinationCards component
-import {
-  XCircleIcon,
-  CheckCircleIcon,
-  Square2StackIcon,
-  ExclamationCircleIcon,
-} from "@heroicons/react/20/solid";
-import { getAge } from "../../utils/getAge";
-import { getDistance } from "../../utils/getDistance";
-
+import DestinationCards from "../../components/DestinationCards"; // Import your DestinationCards component
+import DestinationModal from "../../components/DestinationModal"
 export default function Feed() {
   const { user, handleProfileDetails } = useContext(UserContext);
   const {
@@ -61,6 +42,18 @@ export default function Feed() {
     const data = getSession();
   }, []);
 
+  const [isModal, setModal]  = useState(false);
+  const [modalData, setModalData] = useState();
+  const [modalStatus, setModalStatus] = useState(0);
+  const handlePick = (data, isSimpleClick) => {
+    if (isSimpleClick){
+      // It's just a click, and data is probably not here yet, open model
+      setModal(true);
+    } else {
+      setModalData(data);
+      setModalStatus(1);
+    }
+  }
   const tripData1 = {
     imageUrl: '/Image_of_paris.jpg',
     destination: 'Paris',
@@ -69,6 +62,7 @@ export default function Feed() {
     leastLiked: 'Long museum queues',
     budget: 1200,
     splurgeStatus: 'Yes',
+    choiceHandler: handlePick
   };
 
   const tripData2 = {
@@ -79,6 +73,7 @@ export default function Feed() {
     leastLiked: "Crowded subways",
     budget: "$1500",
     splurgeStatus: "No",
+    choiceHandler: handlePick
   };
 
   const tripData3 = {
@@ -89,6 +84,7 @@ export default function Feed() {
     leastLiked: "Crowded beaches",
     budget: "$900",
     splurgeStatus: "No",
+    choiceHandler: handlePick
   };
 
   const tripData4 = {
@@ -99,6 +95,7 @@ export default function Feed() {
     leastLiked: "Traffic",
     budget: "$2000",
     splurgeStatus: "Yes",
+    choiceHandler: handlePick
   };
 
   const tripData5 = {
@@ -109,6 +106,7 @@ export default function Feed() {
     leastLiked: "Tourist crowds",
     budget: "$1100",
     splurgeStatus: "No",
+    choiceHandler: handlePick
   };
 
   const tripData6 = {
@@ -119,44 +117,17 @@ export default function Feed() {
     leastLiked: "Hot weather",
     budget: "$2500",
     splurgeStatus: "Yes",
+    choiceHandler: handlePick
   };
+
+
 
   return (
     <>
+      { isModal && <DestinationModal data={modalData} status={modalStatus}></DestinationModal>}
       <Navbar page="Feed" />
 
-      {user != null && newMatch != null && (
-        <NewMatch user={user} newMatch={newMatch} />
-      )}
-
-      {user != null && firstClick && musicMatch && (
-        <MusicMatched
-          user={musicMatch}
-          actualUser={user}
-          setMusicMatch={setMusicMatch}
-        />
-      )}
-
-      {!onBoarding && user != null && (
-        <BoardingPage
-          user={user}
-          step={step}
-          setStep={setStep}
-          realLocation={realLocation}
-          setRealLocation={setRealLocation}
-          handleProfileDetails={handleProfileDetails}
-        />
-      )}
-
-      {user != null && onBoarding && !matchesFetching && (
-        <FeedMain
-          user={user}
-          users={potentialMatches}
-          setUsers={setPotentialMatches}
-        />
-      )}
-
-      {/* Add DestinationCards component here */}
+      
       <div className="flex justify-center items-center my-10 space-x-6">        
         <DestinationCards {...tripData1} />
         <DestinationCards {...tripData2} />
@@ -175,108 +146,3 @@ export default function Feed() {
   );
 }
 
-const FeedMain = (props) => {
-  const { user, users, setUsers } = props;
-  const [notification, setNotification] = useState(0);
-  const name = user.name;
-  const img = user.img;
-
-  useEffect(() => {}, [users]);
-
-  useEffect(() => {
-    const notificationsRef = ref(rdb, `Notifications/${user.id}`);
-
-    const unsubscribe = onValue(notificationsRef, (snapshot) => {
-      const data = snapshot.val();
-      if (data !== null) {
-        setNotification(data);
-      }
-    });
-    return () => unsubscribe();
-  }, [users]);
-
-  return (
-    <>
-      <main className="bg-fill col-1 flex h-dvh items-center justify-center overflow-y-auto fold:overflow-auto  sm:overflow-x-hidden md:h-full lg:-overflow-x-hidden">
-        <div className="-mt-10 md:mt-10">
-          <nav className="mt-8 flex items-center justify-between px-5 text-blue-500 lg:hidden">
-            <div className="mt-1 bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-left text-4xl font-semibold text-transparent">
-              SpotChase
-            </div>
-            <div className="flex items-center justify-center gap-1">
-              <Link
-                href={`profile/${user.id}`}
-                className="inline-block cursor-pointer rounded-full bg-gradient-to-t from-blue-500 to-green-500 p-0.5 hover:scale-110"
-              >
-                <img
-                  src={img}
-                  alt={`${name} Photo`}
-                  className="drop-shadow-xs rounded-full object-cover"
-                  style={{ width: "30px", height: "30px" }}
-                />
-              </Link>
-
-              <Link className="relative hover:scale-110" href={`/chat`}>
-                <div
-                  className={`absolute ${notification <= 0 && "hidden"} bottom-5 left-5 inline-flex h-6 w-6 items-center justify-center rounded-full border-2 border-white bg-blue-500 text-xs font-bold text-white shadow-md`}
-                >
-                  {notification}
-                </div>
-                <img
-                  src="./chat.png"
-                  alt="Chat"
-                  className="h-8 w-8 cursor-pointer  fold:hidden sm:block"
-                />
-              </Link>
-              <Link href={`/settings`}>
-                <img
-                  src="./settings.png"
-                  alt="Settings"
-                  className="h-8 w-8 cursor-pointer hover:scale-110 fold:hidden sm:block"
-                />
-              </Link>
-            </div>
-          </nav>
-
-          <nav className="mx-auto hidden items-center justify-between  text-blue-500  lg:flex lg:justify-center lg:gap-20">
-            <div className="ml-5 mt-1 bg-gradient-to-t from-blue-500 to-green-500 bg-clip-text text-left text-4xl font-semibold text-transparent lg:text-6xl">
-              SpotChase
-            </div>
-            <div className="flex items-center justify-center gap-2">
-              <Link
-                href={`profile/${user.id}`}
-                className="inline-block cursor-pointer rounded-full bg-gradient-to-t from-blue-500 to-green-500 p-0.5 hover:scale-110"
-              >
-                <img
-                  src={img}
-                  alt={`${name} Photo`}
-                  className="drop-shadow-xs rounded-full object-cover"
-                  style={{ width: "40px", height: "40px" }}
-                />
-              </Link>
-              <Link href={`/chat`} className="relative hover:scale-110">
-                <div
-                  className={`absolute ${notification <= 0 && "hidden"} bottom-6 left-6 inline-flex h-6 w-6 items-center justify-center rounded-full border-2 border-white bg-blue-500 text-xs font-bold text-white shadow-md`}
-                >
-                  {notification}
-                </div>
-                <img
-                  src="./chat.png"
-                  alt="Chat"
-                  className="h-10 w-10 cursor-pointer"
-                />
-              </Link>
-              <Link href={`/settings`}>
-                <img
-                  src="./settings.png"
-                  alt="Settings"
-                  className="h-10 w-10 cursor-pointer hover:scale-110"
-                />
-              </Link>
-            </div>
-          </nav>
-        </div>
-      </main>
-    </>
-  );
-};
---
+++ Frontend/src/spotchase/components/DestinationModal.jsx
@@ -0,0 +1,59 @@
+function DestinationModal({data, status}){
+    const style = {
+      position: 'fixed',
+      width: '60vw',
+      height: '80vh',
+      left: '20vw',
+      top: '10vh',
+      backgroundColor: '#DDDDDDAA',
+      border: '3px solid black',
+      zIndex: '1'
+    }
+    const regularData = status == 0 ? 
+    
+    (<div style={style}>
+        <p style={{textAlign: 'center'}}>Loading...</p>
+    </div>)
+    : (
+    <div style={style}>
+      <InnerDest data={data} number={0}></InnerDest>
+    </div>
+    );
+      
+    
+    return (
+           regularData
+    );
+  }
+
+  export default DestinationModal;
+
+  function InnerDest({data, number}){
+    console.log("DATA FOllows");
+    console.log(data);
+    var destination, budget, days;
+    var tagline = null;
+    if (data.itineraries){
+        destination = data.itineraries[number].destination
+        budget = data.itineraries[number].budget
+        days =data.itineraries[number].days
+        if (data.itineraries[number].tagline){
+            tagline = data.itineraries[number].tagline;
+        }
+    } else {
+        destination = data[number].destination
+        budget =      data[number].budget
+        days    =     data[number].days
+        if (data[number].tagline){
+            tagline = data.itineraries[number].tagline;
+        }
+    }
+    
+    return (
+        <>
+        <h1> {destination} </h1>
+        <h2> ~{budget} </h2>
+        <h2> {days.length} days </h2>
+        </>
+    )
+  }
