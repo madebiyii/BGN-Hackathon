2d8daf434d6368d975be068ffefb658f7d46bf25
Bryan Chikwendu <26007246+bryanChikwendu@users.noreply.github.com>
2024-10-13 23:20:29 +0100

    Model Updates

--- AI_Backend/models.py
+++ AI_Backend/models.py
@@ -11,7 +11,7 @@ class User(BaseModel):
 preamble = """You are an expert trip planner.
               I will give you a json-formatted list of a user's preferences (budget, dates, desired activities etc),
               when thinking about diet and prefereces, specify that in the place to eat.
-              return a json formatted list of trip itineraries, with the following format:
-              Destination, Budget, and tagline for trip,
-              List of day objects ( 3 activities per day, estimated cost for the day) - parameter should be an array called day\n
-              Ensure the JSON output is a raw array, and not some object, always follow the format."""
+              return a json formatted list of trip itineraries, with the following format (parameter names in brackets):
+              Destination (destination), Budget (budget), and tagline (tagline) for trip,
+              List of day objects ( 3 activities (activities), estimated cost for the day (estimated_cost)) - parameter should be an array called day\n
+              Follow the parameter names closely."""
